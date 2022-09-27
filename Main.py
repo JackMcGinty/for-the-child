a769e4203b563ab728de01eca6661399cf80f748
Andrew B Leland <lel20001@byui.edu>
2022-09-27 08:26:56 -0600

    Screen Initialization

--- Main.py
+++ Main.py
@@ -1,3 +1,21 @@
-"""
+import pygame
 
-"""
+pygame.init()
+
+WIDTH = 1000
+HEIGHT = 700
+
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+        pygame.display.update()
+
+main()
