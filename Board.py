31486f7e61c1ee001d77abfe8a903f97993ff854
BrennonL <104086501+BrennonL@users.noreply.github.com>
2022-10-06 14:40:58 -0600

    added a screen variable in the Board class

--- Board.py
+++ Board.py
@@ -1,3 +1,21 @@
 """
 Brennon
 """
+
+import pygame
+pygame.init()
+
+
+class Board:
+    levels = {
+        "level_1":4,
+        "level_2":8,
+        "level_3":16,
+
+    }
+
+    screen  = pygame.display.set_mode([600,600])
+
+
+        
+
