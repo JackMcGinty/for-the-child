188fa4040edb3bb76a7985270d967a37234477e1
Andrew B Leland <lel20001@byui.edu>
2022-10-20 16:10:21 -0600

    Added collision with mouse click and card

--- Board.py
+++ Board.py
@@ -4,26 +4,33 @@ Brennon
 
 from tkinter import Y
 import pygame
+from Cards import Card, generate_deck
+from Level import *
 #pygame.init()
 
+WIDTH = 1000
+HEIGHT = 700
 
 class Board:
-    levels = {
-        "level_1":4,
-        "level_2":8,
-        "level_3":16,
+    def __init__(self):
+        self.levels = {
+            "level_1":4,
+            "level_2":8,
+            "level_3":16,
 
-    }
+        }
+        self.level = Level()
 
 
 
     def DisplayBoard(self):
         pygame.init()
 
-        WIDTH = 1000
-        HEIGHT = 700
 
         screen = pygame.display.set_mode([WIDTH, HEIGHT])
+
+
+        list_of_cards = generate_deck(4)
 
         #screen = pygame.display.set_mode()
         run = True 
@@ -32,26 +39,30 @@ class Board:
                 if event.type == pygame.QUIT:
                     run = False
 
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    for i in list_of_cards:
+                            if i.rect.collidepoint(event.pos):
+                                print("true")
+                    
                     # Initialing Color
-            color = (255,0,0)
+            # color = (255,0,0)
   
-            Xfirst = 30
-            Yfirst = 30
-            Xsize = 60
-            Ysize = 80
+            # Xfirst = 30
+            # Yfirst = 30
+            # Xsize = 60
+            # Ysize = 80
             # Drawing Rectangle
-            for i in range(34):
+
+            #     pygame.draw.rect(screen, color, pygame.Rect(Xfirst, Yfirst, Xsize, Ysize))
                 
-                pygame.draw.rect(screen, color, pygame.Rect(Xfirst, Yfirst, Xsize, Ysize))
-                
-                Xfirst += 80
-                # if i == 11 or i == 21 or i == 31:
+            #     Xfirst += 80
+            #     # if i == 11 or i == 21 or i == 31:
+            #     #     Yfirst += 100
+            #     #     Xfirst = 30
+
+                # if Xfirst % 830 == 0:
                 #     Yfirst += 100
                 #     Xfirst = 30
-
-                if Xfirst % 830 == 0:
-                    Yfirst += 100
-                    Xfirst = 30
             pygame.display.update()
 
 
