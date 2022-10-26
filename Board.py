d1524324bc66289bd512823f9f1e7149f63a3f93
Andrew B Leland <lel20001@byui.edu>
2022-10-26 15:04:57 -0600

    Cards match, timer, other cool things!

--- Board.py
+++ Board.py
@@ -21,45 +21,76 @@ class Board:
         }
         self.level = Level()
 
-
+    def start_timer(self, timer):
+        timer += 1
+        return timer
 
     """ Bryan has messed around a bit in here """
     def DisplayBoard(self):
         pygame.init()
+        clock = pygame.time.Clock()
 
 
         screen = pygame.display.set_mode([WIDTH, HEIGHT])
 
 
-        list_of_cards = generate_deck(4, (WIDTH, HEIGHT))
+        list_of_cards = generate_deck(16, (WIDTH, HEIGHT))
 
+        cards_revealed = []
+        cards_mismatched = []
         first_card = ""
         clicked_cntr = 0
+        timer = 0
+        timer_on = False
 
         #screen = pygame.display.set_mode()
         run = True 
         click_counter = 0 # for debug
         while run:
+            clock.tick(60)
+
             for event in pygame.event.get():
+
                 if event.type == pygame.QUIT:
                     run = False
 
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    for card in list_of_cards:
-                            if card.rect.collidepoint(event.pos):
-                                clicked_cntr += 1
-                                if clicked_cntr >= 2:
-                                    if first_card == card:
-                                        pass
-                                    elif first_card.is_match(card):
-                                        print('True')
-                                    clicked_cntr = 0
-                                else:
-                                    first_card = card
+                if timer_on == True:
+                    if timer < 40:
+                        timer = self.start_timer(timer)
+                    else:
+                        timer = 0
+                        cards_mismatched.clear()
+                        first_card = ""
+                        clicked_cntr = 0
+                        timer_on = False
+
+                else:
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        for card in list_of_cards:
+                                if card.rect.collidepoint(event.pos):
+                                    clicked_cntr += 1
+                                    if clicked_cntr >= 2:
+                                        if first_card == card:
+                                            pass
+                                        elif first_card.is_match(card):
+                                            cards_revealed.append(first_card)
+                                            cards_revealed.append(card)
+                                            clicked_cntr = 0
+                                        else:
+                                            cards_mismatched.append(first_card)
+                                            cards_mismatched.append(card)
+
+                                            timer_on = True
+
+                                    else:
+                                        first_card = card
          
             # Draw cards
             for card in list_of_cards:
-                screen.blit(card.surf, card.rect)
+                if card == first_card or card in cards_revealed or card in cards_mismatched:
+                    pygame.draw.rect(screen, card.color, card.rect)
+                else:
+                    screen.blit(card.surf, card.rect)
             pygame.display.flip()
                     # Initialing Color
             # color = (255,0,0)
