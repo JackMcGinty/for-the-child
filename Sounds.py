b75fd2d420333ce40b14f9fd39d4df68c9c9df90
BrennonL <104086501+BrennonL@users.noreply.github.com>
2022-11-17 15:01:44 -0700

    added sounds and started the sound class

---
+++ Sounds.py
@@ -0,0 +1,23 @@
+'''
+Brennon Laney
+'''
+
+from pygame import mixer
+
+
+class sound:
+
+    mixer.init()
+    BACKGROUND_MUSIC = mixer.music.load("/assets/background.wav")
+    BELL_ONE = mixer.Sound("/assets/bell1.wav")
+    BELL_TWO = mixer.Sound("/assets/bell2.wav")
+    CHEER = mixer.Sound("/assets/Cheer.wav")
+    CRYING = mixer.Sound("/assets/crying")
+
+    def play_back_ground_music(self):
+        '''
+        Input: self
+        Functionality: Plays the background music using mixer
+        Returns: Void
+        '''
+        self.BACKGROUND_MUSIC.play()
